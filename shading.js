ee2753818e881b6b9cae674979eca87e7b301ff0
Yannik Wailzer <yannik.wailzer@htl.rennweg.at>
2021-12-09 18:48:38 +0100

    Add shading for all faces

--- shading.js
+++ shading.js
@@ -27,30 +27,41 @@ class Vector {
   }
 }
 
+function toRad(degree) {
+  return degree * Math.PI/180;
+}
+
 // positions and normal direction vectors of tiles:
 const tiles = {
     'face-a':           { pos: new Vector(0, 0, 100), normal: new Vector(0, 0, 1) },
     'face-b':           { pos: new Vector(50, 0, 50), normal: new Vector(1, 0, 0) },
     'face-b-mirrored':  { pos: new Vector(-50, 50, 50), normal: new Vector(-1, 0, 0) },
-    'face-c':           { pos: new Vector(0, -25, 75), normal: new Vector(0, 0, 1) },
-    // 'face-d':           { pos: new Vector(50, 50, 50), normal: new Vector() },
-    // 'face-e':           { pos: new Vector(50, 50, 50), normal: new Vector() },
-    // 'face-f':           { pos: new Vector(50, 50, 50), normal: new Vector() },
-    // 'face-d-mirrored':  { pos: new Vector(50, 50, 50), normal: new Vector() },
-    // 'face-e-mirrored':  { pos: new Vector(50, 50, 50), normal: new Vector() },
-    // 'face-f-mirrored':  { pos: new Vector(50, 50, 50), normal: new Vector() },
-    // 'face-g':           { pos: new Vector(50, 50, 50), normal: new Vector() },
-    // 'face-h':           { pos: new Vector(50, 50, 50), normal: new Vector() },
-    // 'face-i':           { pos: new Vector(50, 50, 50), normal: new Vector() },
-    // 'face-j':           { pos: new Vector(50, 50, 50), normal: new Vector() },
-    // 'face-top':         { pos: new Vector(50, 50, 50), normal: new Vector() },
-    // 'button':           { pos: new Vector(50, 50, 50), normal: new Vector() },
+    'face-c':           { pos: new Vector(0, -25, 75), normal: new Vector(0, -1, 0) },
+    'face-d':           { pos: new Vector(92.5, -200, 42.5), normal: new Vector(1, 0, 0) },
+    'face-e':           { pos: new Vector(82.5, -200, 17.5), normal: new Vector(1, 0, 0) },
+    'face-f':           { pos: new Vector(75, -230, 25), normal: new Vector(1, 0, 0) },
+    'face-d-mirrored':  { pos: new Vector(7.5, -200, 42.5), normal: new Vector(-1, 0, 0) },
+    'face-e-mirrored':  { pos: new Vector(17.5, -200, 17.5), normal: new Vector(-1, 0, 0) },
+    'face-f-mirrored':  { pos: new Vector(25, -230, 25), normal: new Vector(-1, 0, 0) },
+    'face-g':           { pos: new Vector(0, -230, 50), normal: new Vector(0, 0, 1) },
+    'face-h':           { pos: new Vector(0, -255, 25), normal: new Vector(0, -1, 0) },
+    'face-i':           { pos: new Vector(0, -200, 42), normal: new Vector(Math.cos(toRad(4)), 0, 1) }, // 4 degrees
+    'face-j':           { pos: new Vector(0, -210, 45), normal: new Vector(0, 1, 0) },
+    'face-top':         { pos: new Vector(30, -34, 80), normal: new Vector(0, -1, 0) },
+    // 'side-1':           { pos: new Vector(50, 50, -10), normal: new Vector() }, // is 'face-top' in button
+    'side-2':           { pos: new Vector(30, -34, 80), normal: new Vector(Math.sin(toRad(2 * 360/8)), 0, Math.cos(toRad(2 * 360/8))) },
+    'side-3':           { pos: new Vector(30, -34, 80), normal: new Vector(Math.sin(toRad(3 * 360/8)), 0, Math.cos(toRad(3 * 360/8))) },
+    'side-4':           { pos: new Vector(30, -34, 80), normal: new Vector(Math.sin(toRad(4 * 360/8)), 0, Math.cos(toRad(4 * 360/8))) },
+    'side-5':           { pos: new Vector(30, -34, 80), normal: new Vector(Math.sin(toRad(5 * 360/8)), 0, Math.cos(toRad(5 * 360/8))) },
+    'side-6':           { pos: new Vector(30, -34, 80), normal: new Vector(Math.sin(toRad(6 * 360/8)), 0, Math.cos(toRad(6 * 360/8))) },
+    'side-7':           { pos: new Vector(30, -34, 80), normal: new Vector(Math.sin(toRad(7 * 360/8)), 0, Math.cos(toRad(7 * 360/8))) },
+    'side-8':           { pos: new Vector(30, -34, 80), normal: new Vector(Math.sin(toRad(8 * 360/8)), 0, Math.cos(toRad(8 * 360/8))) },
+    'side-9':           { pos: new Vector(30, -34, 80), normal: new Vector(Math.sin(toRad(9 * 360/8)), 0, Math.cos(toRad(9 * 360/8))) },
 }
 
 
-function makeShading(light_position) {  // called in script.js
-  const light_direction = (new Vector(0, 0, 0)).sub(light_position).getNormalized();
-  console.log(light_direction);
+function makeShading(light_position) { // called in script.js
+  const light_direction = (new Vector(0, 0, 0)).sub(light_position).getNormalized(); // direction of sun light parallel to everything
 
   Object.entries(tiles)
     .map(([key, val]) => {
@@ -61,10 +72,9 @@ function makeShading(light_position) {  // called in script.js
           .getDotProductWith(light_direction.getNormalized()) /
         light_direction.getLength();
       darkness = (darkness + 1) / 2; // now: 1 = total darkness, 0.5 = medium, 0 = total brightness
-      console.log(darkness);
 
       return {
-        css_var: `--${key}-brightness`, // name of css variable
+        css_var: `--brightness-${key}`, // name of css variable
         brightness: 1 - darkness, // brightness from 0-1
       };
     })
@@ -72,5 +82,3 @@ function makeShading(light_position) {  // called in script.js
       document.body.style.setProperty(css_var, brightness)
     );
 }
-
-makeShading(new Vector(0, 0, 0)); // init
